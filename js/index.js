347b97a76d767512844f61dc985ee04cb3eae1d3
chenlei <1820838212@qq.com>
2019-01-04 19:43:24 +0800

    头部点击操作完成

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
-/**
- * Created by Administrator on 2019/1/4 0004.
- */
+window.addEventListener('DOMContentLoaded',function () {
+   var headerLisNodes = document.querySelectorAll('.nav li');
+   var headerDownNodes = document.querySelectorAll('.nav li .down');
+   var arrowNode = document.querySelector('.arrow');
+
+   arrowNode.style.left = headerLisNodes[0].getBoundingClientRect().left+headerLisNodes[0].offsetWidth/2-arrowNode.offsetWidth/2+'px';
+    headerDownNodes[0].style.width='100%';
+
+    for (var i = 0; i < headerLisNodes.length; i++) {
+        headerLisNodes[i].index=i;
+        headerLisNodes[i].onclick=function () {
+            arrowNode.style.left = this.getBoundingClientRect().left+this.offsetWidth/2-arrowNode.offsetWidth/2+'px';
+            for (var j = 0; j < headerDownNodes.length; j++) {
+                headerDownNodes[j].style.width='0';
+            }
+            headerDownNodes[this.index].style.width='100%';
+        }
+    }
+
+
+
+});
