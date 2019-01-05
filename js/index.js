2adcc8e3dde6c64388b03bb6e04f174cf570faaf
chenlei <1820838212@qq.com>
2019-01-06 00:42:36 +0800

    第二屏完成

--- js/index.js
+++ js/index.js
@@ -5,11 +5,14 @@ window.addEventListener('DOMContentLoaded',function () {
     var contentNode = document.querySelector('.content');
     var contentHeight = contentNode.offsetHeight;
     var contentListNode = document.querySelector('.content-list');
+    var circleLiNodes = document.querySelectorAll('.circle-point li');
+    var homeListNode = document.querySelector('.home-list');
+    var homeListLis = document.querySelectorAll('.home-list li');
     var nowIndex = 0;
-    var wheelTimer = null;
 
-    headerBand();
-    function headerBand() {
+
+    headerHandle();
+    function headerHandle() {
         arrowNode.style.left = headerLisNodes[0].getBoundingClientRect().left+headerLisNodes[0].offsetWidth/2-arrowNode.offsetWidth/2+'px';
         headerDownNodes[0].style.width='100%';
         for (var i = 0; i < headerLisNodes.length; i++) {
@@ -32,8 +35,9 @@ window.addEventListener('DOMContentLoaded',function () {
 
     }
 
-    contentBand();
-    function contentBand() {
+    contentHandle();
+    function contentHandle() {
+        var wheelTimer = null;
         //兼容ie/chrome
         document.onmousewheel = wheel;
         //兼容firefox
@@ -83,12 +87,82 @@ window.addEventListener('DOMContentLoaded',function () {
                         }
                         break;
                 }
-            }, 200)
+                console.log(nowIndex)
+            },200);
 
             //禁止默认行为
             event.preventDefault && event.preventDefault();
             return false;
         }
+
+        //用户缩放浏览器
+        window.onresize = function () {
+            //更新小箭头的位置
+            arrowNode.style.left = headerLisNodes[nowIndex].getBoundingClientRect().left + headerLisNodes[nowIndex].offsetWidth / 2
+                - arrowNode.offsetWidth / 2 + 'px';
+            //更新ul的位置
+            contentListNode.style.top = -nowIndex * contentNode.offsetHeight + 'px';
+        }
+
     }
 
+
+    //第一屏
+    homeHandle();
+    function homeHandle() {
+        var timer = null;
+        var lastIndex = 0;
+        var nowIndex = 0;
+        var lastTime = 0;
+        for (var i = 0; i < circleLiNodes.length; i++) {
+            circleLiNodes[i].index = i;
+            circleLiNodes[i].onclick=function () {
+                clearInterval(timer);
+                nowIndex=this.index;
+                if(nowIndex==lastIndex) return;
+                var nowTime=Date.now();
+                if(nowTime-lastTime<1000) return;
+                for (var j = 0; j < homeListLis.length; j++) {
+                    homeListLis[j].className='commont-title';
+                }
+                if(nowIndex<lastIndex){
+                    homeListLis[lastIndex].className='commont-title right-hide';
+                    homeListLis[nowIndex].className='commont-title left-show';
+                }else if(nowIndex>lastIndex){
+                    homeListLis[lastIndex].className='commont-title left-hide';
+                    homeListLis[nowIndex].className='commont-title right-show';
+                }
+                circleLiNodes[lastIndex].className = '';
+                this.className = 'active';
+                lastIndex=nowIndex;
+                lastTime=nowTime;
+            }
+
+        }
+
+        auto();
+        function auto() {
+            timer = setInterval(function () {
+                nowIndex++;
+                if(nowIndex>=4){
+                    nowIndex = 0;
+                }
+                homeListLis[lastIndex].className='commont-title left-hide';
+                homeListLis[nowIndex].className='commont-title right-show';
+                circleLiNodes[lastIndex].className = '';
+                circleLiNodes[nowIndex].className = 'active';
+                lastIndex=nowIndex;
+            },1500);
+        }
+
+        homeListNode.onmouseenter=function () {
+            clearInterval(timer)
+        }
+        homeListNode.onmouseleave=function () {
+            auto();
+        }
+
+
+
+    }
 });
